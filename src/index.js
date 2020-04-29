7c4e68e9a57e851d36501140f6cedfa66dbbdc1f
Jakapob <jakapobn@gmail.com>
2020-04-29 11:27:29 +0700

    test redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers'
+import logger from 'redux-logger'
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
